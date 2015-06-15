96cf58c7f758a7523837c5b5b25a2ecc30894924
Olivia Cheng <olivia@heyitsolivia.com>
2015-06-15 14:26:11 -0400

    Add js to toggle checkbox when repcards are clicked

--- app/static/js/forms.js
+++ app/static/js/forms.js
@@ -6,4 +6,16 @@ $(document).ready(function () {
         $(this).removeClass('is-gray');
     });
 
+
+    // Toggle checkbox when repbox is clicked
+
+    $('.repcard').on('click', function() {
+        $(this).toggleClass('is-selected');
+
+        $(this).find('.repcard__checkbox').each(function() {
+            var checkbox = $(this);
+            checkbox.prop("checked", !checkbox.prop("checked"));
+        });
+    });
+
 });
